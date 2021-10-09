c2cbbc073c6ce7366da1a2d94635d5cb21cfc61c
Киселев Сергей <s.kiselev@rrost.lan>
2021-10-09 20:26:48 +0300

    Initial Commit

---
+++ PhotoGallery/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  PhotoGallery
+//
+//  Created by Sergey on 09.10.2021.
+//
+
+import UIKit
+
+class  MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    
+        view.backgroundColor = .gray
+    }
+}
