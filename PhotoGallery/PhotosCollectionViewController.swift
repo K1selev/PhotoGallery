c47aba86ff5996a4532b167f4bdcea7ba1349a57
Киселев Сергей <s.kiselev@rrost.lan>
2021-10-11 21:06:32 +0300

    add searching bar

--- PhotoGallery/PhotosCollectionViewController.swift
+++ PhotoGallery/PhotosCollectionViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
+    //lazy инициализируется только когда нажимаем
+    //пустой closure
+    private lazy var addBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped))
+    }()
+    
+    private lazy var actionBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(actionBarButtonTapped))
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,7 +24,19 @@ class PhotosCollectionViewController: UICollectionViewController {
         collectionView.backgroundColor = .orange
         setupNavigationBar()
         setupCollectionView()
+        setupSearchBar()
     }
+    
+    //MARK: - NavigationItems action
+    
+    @objc private func addBarButtonTapped() {
+        print(#function)
+    }
+    @objc private func actionBarButtonTapped() {
+        print(#function)
+    }
+    
+    //MARK: - Setup UI Elements
     
     //функция настройки collectionView
     private func setupCollectionView() {
@@ -29,8 +50,23 @@ class PhotosCollectionViewController: UICollectionViewController {
         titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
         titleLabel.textColor = #colorLiteral(red: 0.502007544, green: 0.4980560541, blue: 0.4979303479, alpha: 1)
         navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        
+        navigationItem.rightBarButtonItems = [actionBarButtonItem, addBarButtonItem]
     
     }
+    
+    //функция настройки SearcController
+    private func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController = searchController
+        //скрытие верхнего бара
+        searchController.hidesNavigationBarDuringPresentation = false
+        //без затемнения
+        searchController.obscuresBackgroundDuringPresentation = false
+    }
+    
+    //MARK: - UICollectionViewDataSource,  UICollectionViewDelegate
     
     //метод отвечает за количество ячеек
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
